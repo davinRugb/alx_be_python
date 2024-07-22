3da92ef99e714899367516520ea6749225d5d065
Davin Rugambwa <davinrugambwa@gmail.com>
2024-07-22 14:48:02 +0200

    OOP 1

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self,title,author,year)
+        self.title = title
+        self.author = author 
+        self.year = year
+
+    def __del__(self):
+        print("Deleting (title of the book)")
+
+    def __str__(self):
+        return f"(title) by (author), published in (year)"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
